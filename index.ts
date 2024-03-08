4cfed14122d2e5e82edc0fae41f85efa5102cd9f
AlizyKhi <162714208+AlizyKhi@users.noreply.github.com>
2024-03-08 19:01:32 +0500

    TYPESCRIPT FILE
    First Assignment HELLO WORLD

---
+++ index.ts
@@ -0,0 +1,12 @@
+let firstName:string = ("HELLO,WORLD");
+console.log(firstName);
+let v:number=800
+let x:number=800
+console.log(v+x)
+let abc='true'
+let profile='false';
+console.log(profile)
+
+
+
+
